727a419fa06c60e159828f50f29dceda568338cc
chaitanyaArige <chaitanyaarige@gmail.com>
2021-08-05 19:49:39 +0530

    clear all errors success

--- api/app.js
+++ api/app.js
@@ -3,11 +3,43 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require('mongoose');
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+const dbUrl = 'mongodb+srv://chai:chai@chaidbcluster.ohmfe.mongodb.net/myFirstDatabase?retryWrites=true&w=majority/meetingScheduler/appointments'
+
+mongoose.Promise = global.Promise;
+//Adds connection to database using mongoose
+//for <dbuser>:replace with your username, <dbpassword>: replace with your password.
+//<DATABASE_URL>: replace with database url, example:ds234562.mlab.com:17283
+
+
+mongoose.connect(dbUrl, {useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
+  if (err)
+      console.error(err);
+  else
+      console.log("Connected to the mongodb"); 
+});
+
+
+//This enabled CORS, Cross-origin resource sharing (CORS) is a mechanism that allows restricted resources (e.g. fonts) 
+//on a web page to be requested from another domain outside the domain from which the first resource was served
+app.all('/*', function(req, res, next) {
+  // CORS headers
+  res.header("Access-Control-Allow-Origin", "*"); // restrict it to the required domain
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  // Set custom headers for CORS
+  res.header('Access-Control-Allow-Headers', 'Content-type,Accept,X-Access-Token,X-Key');
+  if (req.method == 'OPTIONS') {
+    res.status(200).end();
+  } else {
+    next();
+  }
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- api/routes/index.js
+++ api/routes/index.js
@@ -1,5 +1,12 @@
 var express = require('express');
 var router = express.Router();
+const appointmentController = require('../src/controllers/appointments')
+const slotController = require('../src/controllers/slot')
+
+
+router.get('/appointments', appointmentController.all);
+router.get('/retrieveSlots', slotController.all);
+router.post('/appointmentCreate', appointmentController.create);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -7,3 +14,4 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
+
